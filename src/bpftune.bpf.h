e98409384a46c73af5d6772b0cb9f57686e253e1
Alan Maguire <alan.maguire@oracle.com>
2022-05-26 16:51:37 +0100

    remove __bpf_printk()s when not in DEBUG

--- src/bpftune.bpf.h
+++ src/bpftune.bpf.h
@@ -8,11 +8,16 @@
 
 #include "bpftune.h"
 
-
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+
+/* don't want __bpf_printk()s slipping into production... */
+#ifndef DEBUG
+#undef __bpf_printk
+#define __bpf_printk(...)
+#endif
 
 struct {
         __uint(type, BPF_MAP_TYPE_RINGBUF);
