f7b6a768e0b3427929260742340cafc809b4d3c8
Alan Maguire <alan.maguire@oracle.com>
2023-03-03 21:23:31 +0000

    add probe.bpf.c

---
+++ src/probe.bpf.c
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+/* Copyright (c) 2023, Oracle and/or its affiliates. */
+
+#include "bpftune.bpf.h"
+#include "netns_tuner.h"
+
+/* probe hash map */
+BPF_MAP_DEF(probe_hash_map, BPF_MAP_TYPE_HASH, __u64, __u64, 65536);
+
+/* probe kprobe/fentry */
+BPF_FENTRY(setup_net, struct net *net, struct user_namespace *user_ns)
+{
+	return 0;
+}
+
+#ifdef BPFTUNE_LEGACY
+SEC("raw_tracepoint/neigh_create")
+int BPF_PROG(bpftune_neigh_create, struct neigh_table *tbl,
+             struct net_device *dev, const void *pkey,
+             struct neighbour *n, bool exempt_from_gc)
+#else
+SEC("tp_btf/neigh_create")
+int BPF_PROG(bpftune_neigh_create, struct neigh_table *tbl,
+             struct net_device *dev, const void *pkey,
+             struct neighbour *n, bool exempt_from_gc)
+#endif
+{
+	return 0;
+}
+
+SEC("cgroup/sysctl")
+int sysctl_write(struct bpf_sysctl *ctx)
+{
+        return 1;
+}
