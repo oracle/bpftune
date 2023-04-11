134ed741e51101ca137f2e00b294d567fcaebbae
Alan Maguire <alan.maguire@oracle.com>
2023-04-11 18:30:58 +0100

    use _DEFAULT_SOURCE as per warnings

--- src/bpftune.c
+++ src/bpftune.c
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /* Copyright (c) 2023, Oracle and/or its affiliates. */
 
-#define _BSD_SOURCE
+#define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
