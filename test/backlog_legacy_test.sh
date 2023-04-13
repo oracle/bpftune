49bc44f87a22059a973d55e966e76eed84450b07
Alan Maguire <alan.maguire@oracle.com>
2023-04-13 17:34:21 +0100

    fix legacy option in backlog legacy test
    should be "-L"

--- test/backlog_legacy_test.sh
+++ test/backlog_legacy_test.sh
@@ -55,7 +55,7 @@ for FAMILY in ipv4 ipv6 ; do
 	echo "Running ${MODE}..."
 	test_run_cmd_local "ip netns exec $NETNS $IPERF3 -s -p $PORT -1 &"
 	if [[ $MODE != "baseline" ]]; then
-		test_run_cmd_local "$BPFTUNE -l &"
+		test_run_cmd_local "$BPFTUNE -L &"
 		sleep $SETUPTIME
 	else
 		LOGSZ=$(wc -l $LOGFILE | awk '{print $1}')
